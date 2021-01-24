2f62acfd5d89e5cb0fac08c36691466b9cb25bf9
rfminguez <rfminguez@gmail.com>
2021-01-24 21:09:57 +0100

    update: divide path input in two, one for directory and another for filename

--- p_reporting/m_report_to_csv.py
+++ p_reporting/m_report_to_csv.py
@@ -1,3 +1,7 @@
-def df_to_csv(df, path="data/results/report.csv"):
+import os
+
+
+def df_to_csv(df, directory, filename):
+    path = os.path.join(directory, filename)
     print(f"Saving file to {path}")
     df.to_csv(path, index=False)
